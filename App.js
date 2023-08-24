020e04a103d169f8192a8dbd188f5f59918e5f80
ProfMarceloIfal <marcelo.almeida@ifal.edu.br>
2023-08-24 17:01:01 -0300

    Commit 1

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Principal } from './src/Componentes/Principal';
+import { Tela2 } from './src/Componentes/Tela2';
+import { Tela3 } from './src/Componentes/Tela3';
+import { Tela4 } from './src/Componentes/Tela4';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen name='Principal' component={Principal} />
+        <Stack.Screen name='Tela_2' component={Tela2} />
+        <Stack.Screen name='Tela_3' component={Tela3} />
+        <Stack.Screen name='Tela_4' component={Tela4} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
